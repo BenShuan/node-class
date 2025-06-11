6aa20ac0588930395f09929999ccdacdaced571c
Ben Shuan <benshuan@gmail.com>
2025-06-11 12:44:13 +0300

    update files sturcture

--- 1-run-node/run-node.js
+++ 1-run-node/run-node.js
@@ -1,1 +1,7 @@
+/**
+ * @fileOverview This file demonstrates a simple Node.js script.
+ * @description To run this file, execute `node <filename.js>` in your terminal.
+ * For example: `node run-node.js`
+ */
+
 console.log("My first node scirpt");
